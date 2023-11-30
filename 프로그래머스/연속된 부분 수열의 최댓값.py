a7ea706c812d656fe4b6d9633a7584a879dfd866
OhHyukJun <kmh7277@naver.com>
2023-11-30 16:41:51 +0900

    연속된 부분 수열의 최댓값

---
+++ 프로그래머스/연속된 부분 수열의 최댓값.py
@@ -0,0 +1,16 @@
+def solution(sequence,k):
+    left = right = 0
+    answer = [0, len(sequence)]
+    sum = sequence[left]
+    while True:
+        if sum < k:
+            right += 1
+            if right == len(sequence):
+                break
+            sum += sequence[right]
+        else:
+            if sum == k and right-left < answer[1]-answer[0]:
+                answer = [left,right]
+            sum -= sequence[left]
+            left += 1
+    return answer
