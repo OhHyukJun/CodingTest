347b696dd90cc8e5f7d7a33b3d4b6640c8475b5e
OhHyukJun <kmh7277@naver.com>
2024-02-08 11:34:56 +0900

    Feat: 12865

--- 백준 풀이/12865.py
+++ 백준 풀이/12865.py
@@ -1,46 +1,13 @@
-'''
-N개의 물건
-무게 W 가치 V
-무게 K는 배낭의 최대값
-첫줄 1<=N<=100, 1<=K<=100,000
-N개의 줄에 1<=W<=100,000,1<=V<=1,000 
-4 7
-6 13
-4 8
-3 6
-5 12
-
-14
-'''
-import itertools
-import sys
-#input = sys.stdin.readline()
-
-N,K = map(int,input().split())
-
-arr = {}
-W_arr = []
-for _ in range(N):
-    W, V = map(int,input().split())
-    arr[W] = V
-    W_arr.append(W)
-''''
+N, K = map(int,input().split())
 arr = []
-W_arr = []
-for _ in range(N):
+for i in range(N):
     W, V = map(int,input().split())
     arr.append((W,V))
-    W_arr.append(W)
-'''
-List = []
-for i in range(1,len(W_arr)+1):
-    for temp in itertools.combinations(W_arr, i):
-        if sum(temp) <= K:
-            values = [arr[weight] for weight in temp]  # temp의 각 요소에 해당하는 가치를 리스트로 만듦
-            if len(temp) == 1:
-                List.extend(values)  # values 리스트 전체를 List에 추가
-            else: List.append(sum(values))
+arr.sort(reverse=True)
+dp = [0 for _ in range(K+1)]
 
-print(max(List))
+for W,V in arr:
+    for j in range(K,W-1,-1):
+        dp[j] = max(dp[j],dp[j-W] + V)
 
-
+print(max(dp))
