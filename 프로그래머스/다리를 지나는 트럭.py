bdf29b82f52ad672dd157284ee26ed1308d215d8
OhHyukJun <kmh7277@naver.com>
2024-02-28 12:11:42 +0900

    Feat: 다리를 지나는 트럭

---
+++ 프로그래머스/다리를 지나는 트럭.py
@@ -0,0 +1,25 @@
+from collections import deque
+def solution(bridge_length, weight, truck_weights):
+    answer = 0
+    bridge = deque([0] * bridge_length)
+    truck_weights = deque(truck_weights)
+    current_weight = 0
+
+    while len(truck_weights):
+        answer += 1
+        current_weight -= bridge.popleft()
+        if current_weight + truck_weights[0] <= weight:
+            current_weight += truck_weights[0]
+            bridge.append(truck_weights.popleft())
+        else:
+            bridge.append(0)
+    
+    answer += bridge_length
+    print(answer)
+    return answer
+
+bridge_length = 2
+weight = 10
+truck_weights = [7,4,5,6]
+
+solution(bridge_length,weight,truck_weights)
