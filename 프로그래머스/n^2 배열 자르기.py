6ab341b2b79409f9205ceb528256f88290ec247d
OhHyukJun <kmh7277@naver.com>
2024-05-04 17:18:03 +0900

    Feat: n^2 배열 자르기

---
+++ 프로그래머스/n^2 배열 자르기.py
@@ -0,0 +1,15 @@
+#left right 위주로 풀기!
+def solution(n, left, right):
+    answer = []
+    arr = []
+    for i in range(left,right+1):
+        if i < n:
+            answer.append(i+1)
+        elif i%n <= i//n:
+            answer.append(i//n+1)
+        else:
+            answer.append(i%n+1)
+            #print(i%n,i//n)
+            #print(i//n+1)
+        
+    return answer
