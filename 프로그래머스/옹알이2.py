faeef53a5a8315cabe2e5e9b87ea97b543af4830
OhHyukJun <kmh7277@naver.com>
2025-03-17 18:13:39 +0900

    Feat: 옹알이 2

---
+++ 프로그래머스/옹알이2.py
@@ -0,0 +1,21 @@
+def solution(babbling):
+    answer = 0
+    arr = ["aya", "ye", "woo", "ma"]
+    i=0
+    while i < len(babbling):
+        for j in range(4):
+            if arr[j]*2 in babbling[i]:
+                continue
+            if arr[j] in babbling[i]:
+                babbling[i]=babbling[i].replace(arr[j],' ')
+                
+        if babbling[i] not in arr:
+            i += 1
+        else:
+            i += 1
+    
+    for i in babbling:
+        if i.isspace() == True:
+            answer += 1
+
+    return answer
