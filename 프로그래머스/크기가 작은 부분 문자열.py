63d2e5518082bd689ba88be2d35990c0f3fd534b
OhHyukJun <kmh7277@naver.com>
2025-03-25 19:59:04 +0900

    Feat: 크기가 작은 문자열

---
+++ 프로그래머스/크기가 작은 부분 문자열.py
@@ -0,0 +1,14 @@
+
+    
+def solution(t, p):
+    answer = 0
+    arr = []
+    for i in range(len(t)):
+        if len(t[i:i+len(p)]) == len(p):
+            arr.append(int(t[i:i+len(p)]))
+    # print(len(p))
+    for i in arr:
+        if i <= int(p):
+            answer += 1
+
+    return answer
